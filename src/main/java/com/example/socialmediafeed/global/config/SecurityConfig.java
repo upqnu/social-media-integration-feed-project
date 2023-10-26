845df01c509b5ff56669657b5cf676c89df3d3a1
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-10-27 02:03:50 +0900

    Fix #23: SecurityConfig 오류 수정

--- src/main/java/com/example/socialmediafeed/global/config/SecurityConfig.java
+++ src/main/java/com/example/socialmediafeed/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -14,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable()
                 .authorizeHttpRequests(authorize ->
-                        authorize.requestMatchers("/**").permitAll()
+                        authorize.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
                 );
         return http.build();
     }
