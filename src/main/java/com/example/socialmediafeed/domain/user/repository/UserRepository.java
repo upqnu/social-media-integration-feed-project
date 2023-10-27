ed7ee97cdaafa59f52a66013c4fb1cd6419ef5be
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-10-27 12:11:22 +0900

    Feat: Jwt 로그인 진행 중

--- src/main/java/com/example/socialmediafeed/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/socialmediafeed/domain/user/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.socialmediafeed.domain.user.repository;
 import com.example.socialmediafeed.domain.user.entitiy.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     boolean existsByCertificationNumber(String certificationNumber);
     boolean existsByEmail(String email);
 }
--- src/main/java/com/example/socialmediafeed/global/config/SecurityConfig.java
+++ src/main/java/com/example/socialmediafeed/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.socialmediafeed.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable()
+        http.csrf().disable()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
                 );
