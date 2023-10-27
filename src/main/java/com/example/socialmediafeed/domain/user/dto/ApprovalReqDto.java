c4e4194a7c8429112d76dc9e94eec0898ccaad3a
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-10-27 13:20:20 +0900

    Test #32: Approval test 코드 작성

--- src/main/java/com/example/socialmediafeed/domain/user/dto/ApprovalReqDto.java
+++ src/main/java/com/example/socialmediafeed/domain/user/dto/ApprovalReqDto.java
@@ -1,12 +1,20 @@
 package com.example.socialmediafeed.domain.user.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApprovalReqDto {
     @NotEmpty(message = "인증번호를 입력해주세요")
     private String certificationNumber;
+
+    public static ApprovalReqDto from(String certificationNumber) {
+        return new ApprovalReqDto(certificationNumber);
+    }
+
 }
---
+++ src/test/java/com/example/socialmediafeed/domain/UserSetup.java
@@ -0,0 +1,29 @@
+package com.example.socialmediafeed.domain;
+
+import com.example.socialmediafeed.domain.hashtag.entity.Hashtag;
+import com.example.socialmediafeed.domain.user.entitiy.User;
+import com.example.socialmediafeed.domain.user.repository.UserRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSetup {
+    private final UserRepository userRepository;
+
+    public UserSetup(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User build(Hashtag hashtag) {
+        return userRepository.save(buildUser("test@email.com", hashtag));
+    }
+
+    public User buildUser(String email, Hashtag hashtag) {
+        return User.builder()
+                .email(email)
+                .password("1q2w3e4r!")
+                .certificationNumber(RandomStringUtils.random(5, true, true))
+                .hashtag(hashtag)
+                .build();
+    }
+}
