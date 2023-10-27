b682a2a790ec60bcdfb043dbe79762b1da06bc9c
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-10-27 10:44:20 +0900

    Fix #27: Post 오류 수정
    - PostHashtag 연관관계 수정
    - CreatedTime, ModifiedTime 오류 수정
    - JPAAuditing 재수정

--- src/main/java/com/example/socialmediafeed/domain/post/service/PostService.java
+++ src/main/java/com/example/socialmediafeed/domain/post/service/PostService.java
@@ -6,8 +6,6 @@ import com.example.socialmediafeed.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,7 +19,6 @@ public class PostService {
     }
 
     private List<Post> createDummies() {
-        List<Post> dummyPosts = new ArrayList<>();
 
         Post postFBa = Post.builder()
                 .contentId("fb1")
@@ -31,8 +28,6 @@ public class PostService {
                 .viewCount(2)
                 .likeCount(0)
                 .shareCount(3)
-                .createdAt(LocalDateTime.parse("2023-10-01 14:14:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-01 14:15:15"))
                 .build();
 
         Post postFBb = Post.builder()
@@ -43,8 +38,6 @@ public class PostService {
                 .viewCount(12)
                 .likeCount(10)
                 .shareCount(13)
-                .createdAt(LocalDateTime.parse("2023-10-04 17:17:17"))
-                .updatedAt(LocalDateTime.parse("2023-10-04 17:18:18"))
                 .build();
 
         Post postFBc = Post.builder()
@@ -55,8 +48,6 @@ public class PostService {
                 .viewCount(102)
                 .likeCount(77)
                 .shareCount(32)
-                .createdAt(LocalDateTime.parse("2023-10-11 11:11:11"))
-                .updatedAt(LocalDateTime.parse("2023-10-11 11:12:12"))
                 .build();
 
         Post postFBd = Post.builder()
@@ -67,8 +58,6 @@ public class PostService {
                 .viewCount(0)
                 .likeCount(0)
                 .shareCount(0)
-                .createdAt(LocalDateTime.parse("2023-10-18 01:14:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-18 01:18:18"))
                 .build();
 
         Post postFBe = Post.builder()
@@ -79,8 +68,6 @@ public class PostService {
                 .viewCount(827)
                 .likeCount(155)
                 .shareCount(101)
-                .createdAt(LocalDateTime.parse("2023-10-25 09:23:24"))
-                .updatedAt(LocalDateTime.parse("2023-10-25 09:33:35"))
                 .build();
 
         Post postTWa = Post.builder()
@@ -91,8 +78,6 @@ public class PostService {
                 .viewCount(77)
                 .likeCount(30)
                 .shareCount(55)
-                .createdAt(LocalDateTime.parse("2023-10-01 21:14:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-01 21:40:15"))
                 .build();
 
         Post postTWb = Post.builder()
@@ -103,8 +88,6 @@ public class PostService {
                 .viewCount(123)
                 .likeCount(103)
                 .shareCount(131)
-                .createdAt(LocalDateTime.parse("2023-10-06 17:00:17"))
-                .updatedAt(LocalDateTime.parse("2023-10-06 17:44:18"))
                 .build();
 
         Post postTWc = Post.builder()
@@ -115,8 +98,6 @@ public class PostService {
                 .viewCount(13302)
                 .likeCount(7147)
                 .shareCount(3242)
-                .createdAt(LocalDateTime.parse("2023-10-13 23:11:11"))
-                .updatedAt(LocalDateTime.parse("2023-10-13 23:22:22"))
                 .build();
 
         Post postTWd = Post.builder()
@@ -127,8 +108,6 @@ public class PostService {
                 .viewCount(11)
                 .likeCount(2)
                 .shareCount(4)
-                .createdAt(LocalDateTime.parse("2023-10-20 23:54:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-21 00:12:12"))
                 .build();
 
         Post postTWe = Post.builder()
@@ -139,8 +118,6 @@ public class PostService {
                 .viewCount(427)
                 .likeCount(135)
                 .shareCount(2)
-                .createdAt(LocalDateTime.parse("2023-10-25 19:43:24"))
-                .updatedAt(LocalDateTime.parse("2023-10-25 19:53:35"))
                 .build();
 
         Post postIGa = Post.builder()
@@ -151,8 +128,6 @@ public class PostService {
                 .viewCount(21)
                 .likeCount(3)
                 .shareCount(1)
-                .createdAt(LocalDateTime.parse("2023-10-02 04:24:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-02 04:25:15"))
                 .build();
 
         Post postIGb = Post.builder()
@@ -163,8 +138,6 @@ public class PostService {
                 .viewCount(912)
                 .likeCount(190)
                 .shareCount(103)
-                .createdAt(LocalDateTime.parse("2023-10-07 07:17:12"))
-                .updatedAt(LocalDateTime.parse("2023-10-07 07:28:12"))
                 .build();
 
         Post postIGc = Post.builder()
@@ -175,8 +148,6 @@ public class PostService {
                 .viewCount(333)
                 .likeCount(222)
                 .shareCount(111)
-                .createdAt(LocalDateTime.parse("2023-10-13 23:59:11"))
-                .updatedAt(LocalDateTime.parse("2023-10-14 00:11:12"))
                 .build();
 
         Post postIGd = Post.builder()
@@ -187,8 +158,6 @@ public class PostService {
                 .viewCount(200)
                 .likeCount(0)
                 .shareCount(0)
-                .createdAt(LocalDateTime.parse("2023-10-17 21:44:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-17 21:48:18"))
                 .build();
 
         Post postIGe = Post.builder()
@@ -199,8 +168,6 @@ public class PostService {
                 .viewCount(827)
                 .likeCount(155)
                 .shareCount(101)
-                .createdAt(LocalDateTime.parse("2023-10-25 09:23:24"))
-                .updatedAt(LocalDateTime.parse("2023-10-25 09:33:35"))
                 .build();
 
         Post postTHa = Post.builder()
@@ -211,8 +178,6 @@ public class PostService {
                 .viewCount(3)
                 .likeCount(2)
                 .shareCount(1)
-                .createdAt(LocalDateTime.parse("2023-10-03 09:24:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-03 09:25:15"))
                 .build();
 
         Post postTHb = Post.builder()
@@ -223,8 +188,6 @@ public class PostService {
                 .viewCount(12345)
                 .likeCount(1234)
                 .shareCount(123)
-                .createdAt(LocalDateTime.parse("2023-10-09 17:17:12"))
-                .updatedAt(LocalDateTime.parse("2023-10-09 17:28:12"))
                 .build();
 
         Post postTHc = Post.builder()
@@ -235,8 +198,6 @@ public class PostService {
                 .viewCount(9622)
                 .likeCount(2)
                 .shareCount(0)
-                .createdAt(LocalDateTime.parse("2023-10-15 20:19:11"))
-                .updatedAt(LocalDateTime.parse("2023-10-15 20:21:12"))
                 .build();
 
         Post postTHd = Post.builder()
@@ -247,8 +208,6 @@ public class PostService {
                 .viewCount(62)
                 .likeCount(55)
                 .shareCount(43)
-                .createdAt(LocalDateTime.parse("2023-10-20 01:44:14"))
-                .updatedAt(LocalDateTime.parse("2023-10-20 01:48:18"))
                 .build();
 
         Post postTHe = Post.builder()
@@ -259,15 +218,11 @@ public class PostService {
                 .viewCount(12)
                 .likeCount(3)
                 .shareCount(2)
-                .createdAt(LocalDateTime.parse("2023-10-24 19:23:24"))
-                .updatedAt(LocalDateTime.parse("2023-10-24 19:33:35"))
                 .build();
 
-        dummyPosts.addAll(postRepository.saveAll(List.of(
+        return postRepository.saveAll(List.of(
                 postFBa, postFBb, postFBc, postFBd, postFBe, postTWa, postTWb, postTWc, postTWd, postTWe,
                 postIGa, postIGb, postIGc, postIGd, postIGe, postTHa, postTHb, postTHc, postTHd, postTHe
-        )));
-
-        return dummyPosts;
+        ));
     }
 }
