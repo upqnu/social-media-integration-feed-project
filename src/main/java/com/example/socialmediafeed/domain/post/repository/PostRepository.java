3ded4cfa968f703366a28c6a1de2385f9d3892d5
joonyoungyun <101033614+upqnu@users.noreply.github.com>
2023-10-28 22:14:59 +0900

    Refactor #34 : 게시물 목록 - repository 수정
    -repository 선언부에 QuerydslPredicateExecutor 인터페이스를 상속
    - queryDSL 문법에 맞게 @Query 작성
    - hashtag 매개변수에 따라 게시물을 검색하고, List<Post>로 검색 결과를 반환

--- src/main/java/com/example/socialmediafeed/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/socialmediafeed/domain/post/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.example.socialmediafeed.domain.post.repository;
 
 import com.example.socialmediafeed.domain.post.entity.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.List;
 
-    Page<Post>  findByContentContainingIgnoreCase(String hashtag, Pageable pageable);
+public interface PostRepository extends JpaRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
+
+    @Query("SELECT p FROM Post p WHERE lower(p.content) LIKE %:hashtag% ESCAPE '!'")
+    List<Post> findByContentContainingHashtag(@Param("hashtag") String hashtag);
 }
