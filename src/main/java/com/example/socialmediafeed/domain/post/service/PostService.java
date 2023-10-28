8c1132e0b3a3130f80d0bd0b03b9f19f98c2a3eb
joonyoungyun <101033614+upqnu@users.noreply.github.com>
2023-10-28 22:49:55 +0900

    Refactor #34 : 게시물 목록 - service 수정
    - controller에 작성되어 있던 검색 및 pagination 로직을 옮겨서 작성
    - EntityManager 주입받아 JPAQuery 객체를 생성하여 검색어에 따른 검색결과를 찾는 동적 쿼리를 생성하고 실행

--- src/main/java/com/example/socialmediafeed/domain/post/service/PostService.java
+++ src/main/java/com/example/socialmediafeed/domain/post/service/PostService.java
@@ -1,27 +1,61 @@
 package com.example.socialmediafeed.domain.post.service;
 
 import com.example.socialmediafeed.domain.post.entity.Post;
+import com.example.socialmediafeed.domain.post.entity.QPost;
 import com.example.socialmediafeed.domain.post.repository.PostRepository;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
+
+import static com.example.socialmediafeed.domain.post.entity.QPost.post;
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
 
+    private final EntityManager entityManager;
     private final PostRepository postRepository;
 
-    public List<Post> getAllPosts() {
-        List<Post> allPosts = postRepository.findAll();
-        return allPosts;
-    }
+    public Page<Post> getPosts(String hashtag, int page, int page_count, String sortBy, String sortOrder) {
 
-    public Page<Post> findPostByTitleAndContent(String hashtag, Pageable pageable) {
-        return postRepository.findByContentContainingIgnoreCase(hashtag, pageable);
-    }
+        QPost qPost = post;
+        BooleanBuilder builder = new BooleanBuilder();
 
+        if (hashtag != null && !hashtag.isEmpty()) {
+            builder.and(qPost.content.toLowerCase().containsIgnoreCase("#" + hashtag.toLowerCase()));
+        }
+
+        Sort.Direction direction = Sort.Direction.fromString(sortOrder);
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, page_count, sort);
+
+        JPAQuery<Post> query = new JPAQuery<>(entityManager);
+
+        query.from(qPost)
+                .where(builder)
+                .select(qPost);
+
+        List<Post> results = query.fetch();
+
+        int startIndex = (int) pageable.getOffset();
+        int endIndex = Math.min((startIndex + pageable.getPageSize()), results.size());
+
+        if (startIndex > results.size() || startIndex < 0) {
+            // Handle the case where startIndex is out of bounds
+            return new PageImpl<>(Collections.emptyList(), pageable, 0);
+        }
+
+        Page<Post> pageResults = new PageImpl<>(results.subList(startIndex, endIndex), pageable, results.size());
+
+        return pageResults;
 }
