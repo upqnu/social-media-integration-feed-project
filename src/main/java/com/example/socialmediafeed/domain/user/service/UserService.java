b1e68c73b69bad0e35205f6c2b44af97b7ac9ff2
gabang2 <offbeat1020@naver.com>
2023-10-31 01:40:31 +0900

    Feat #25 : JWT Token 기반 통계 검색 기능 구현

--- src/main/java/com/example/socialmediafeed/domain/user/service/UserService.java
+++ src/main/java/com/example/socialmediafeed/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.socialmediafeed.domain.user.service;
 
 import com.example.socialmediafeed.domain.user.entitiy.User;
 import com.example.socialmediafeed.domain.user.repository.UserRepository;
+import com.example.socialmediafeed.global.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
 
     // todo: exception을 business exception으로 갈아야 합니다.
     public User findUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected userId"));
+    }
+
+    /**
+     * User 정보 얻기
+     * @param token
+     * @return
+     */
+    public User findUserByAccessToken(String token) {
+        return findUserById(tokenProvider.getUserId(token));
     }
 }
