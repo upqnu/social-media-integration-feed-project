19be3f4b76f24a17f1189a13e790935ee622fa6b
joonyoungyun <101033614+upqnu@users.noreply.github.com>
2023-10-30 11:25:30 +0900

    Refactor #34 : 게시물 목록 - PostControllerTest 수정
    - Test 클래스에 선언된 불필요한 4개의 annotation 삭제(상속받는 IntegrationTest에 이미 선언되어 있음)

--- src/test/java/com/example/socialmediafeed/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/example/socialmediafeed/domain/post/controller/PostControllerTest.java
@@ -4,23 +4,16 @@ import com.example.socialmediafeed.IntegrationTest;
 import com.example.socialmediafeed.SocialMediaFeedApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SocialMediaFeedApplication.class)
-@AutoConfigureMockMvc
-@Transactional
 class PostControllerTest extends IntegrationTest {
 
     @Test
